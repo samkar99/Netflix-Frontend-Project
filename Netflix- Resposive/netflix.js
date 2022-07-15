89457e5babf0bbc1b3e0433d3b0cd081f2c52d0f
samkar99 <109365724+samkar99@users.noreply.github.com>
2022-07-15 21:24:05 +0530

    Add files via upload

---
+++ Netflix- Resposive/netflix.js
@@ -0,0 +1,93 @@
+$(document).ready(function () {
+
+
+// first-block
+
+  $('.plus1').click(function () {
+        $(".js-data1").slideToggle(500)
+        $(".sx-br1").slideToggle(500)
+    })
+
+// first-block ends
+
+
+
+
+
+
+
+
+
+
+
+
+// second block
+
+$('.plus2').click(function () {
+    $(".js-data2").slideToggle(500)
+    $(".sx-br2").slideToggle(500)
+})
+
+// second block ends
+
+
+
+
+
+
+
+// third block
+
+$('.plus3').click(function () {
+    $(".js-data3").slideToggle(500)
+    $(".sx-br3").slideToggle(500)
+})
+
+// third block ends
+
+  
+
+
+
+
+
+//fourth block
+
+$('.plus4').click(function () {
+    $(".js-data4").slideToggle(500)
+    $(".sx-br4").slideToggle(500)
+})
+
+//fourth block ends
+
+
+
+
+
+
+
+
+
+
+//fifth block
+
+$('.plus5').click(function () {
+    $(".js-data5").slideToggle(500)
+    $(".sx-br5").slideToggle(500)
+})
+
+//fifth blocke ends
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
